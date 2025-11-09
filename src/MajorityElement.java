10ebf21d0d2df8fd94dd68baa7ee0cc54af0fbdc
João Martins <joaogasparlopesmartins@gmail.com>
2025-11-09 13:24:01 +0000

    solution O(n) time O(1) space

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,22 +1,30 @@
-import java.util.Collections;
-import java.util.HashMap;
-
 public class MajorityElement {
 	
 	public int majorityElement(int[] nums) {
 		// Solution for https://leetcode.com/problems/majority-element
-		var counter = new HashMap<Integer, Integer>();
+//		var counter = new HashMap<Integer, Integer>();
+//		
+//		for (int num : nums) {
+//			counter.put(num, counter.getOrDefault(num, 0) + 1);
+//		}
+//		
+//		return Collections.max(counter.keySet());
+		int count = 0;
+		int candidate = 0;
 		
 		for (int num : nums) {
-			counter.put(num, counter.getOrDefault(num, 0) + 1);
+			if (count == 0) {
+				candidate = num;
+			}
+			count += (num == candidate) ? 1 : -1;
 		}
 		
-		return Collections.max(counter.keySet());
+		return candidate;
     }
 
 	public static void main(String[] args) {
 		var me = new MajorityElement();
-		System.out.println(me.majorityElement(new int[] {3,2,3}));
+		System.out.println(me.majorityElement(new int[] {2,2,1,1,1,2,2}));
 
 	}
 
