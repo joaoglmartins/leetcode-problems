6a8f06ac67ae52191123745e0a43f8859fefcd1d
João Martins <joaogasparlopesmartins@gmail.com>
2025-11-10 18:49:40 +0000

    solution for the edge case where k is divisor of nums length

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,19 +1,29 @@
+import java.util.Arrays;
+
 public class RotateArray {
 	
 	public void rotate(int[] nums, int k) {
-		int number = nums[0];
-		int tempNumber = 0;
-		int newIndex = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (newIndex + k >= nums.length) {
-				newIndex = k - nums.length + newIndex;
-			} else {
-				newIndex = newIndex + k;
-			}
-			tempNumber = nums[newIndex];
-			nums[newIndex] = number;
-			number = tempNumber;
-		}
+		int n = nums.length;
+	    k = k % n;
+	    if (k == 0) return;
+
+	    int count = 0;
+
+	    for (int start = 0; count < n; start++) {
+	        int current = start;
+	        int number = nums[start];
+
+	        do {
+	            int newIndex = (current + k) % n;
+	            int tempNumber = nums[newIndex];
+	            nums[newIndex] = number;
+	            number = tempNumber;
+	            current = newIndex;
+	            count++;
+	        } while (start != current);
+	    }
+	    
+	    System.out.println(Arrays.toString(nums));
     }
 
 	public static void main(String[] args) {
