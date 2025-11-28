0528a03546a5843e1be29d324a4df0489426967a
João Martins <joaogasparlopesmartins@gmail.com>
2025-11-28 17:54:06 +0000

    solution O(n2) time O(1) space

---
+++ src/FindIndexFirstOccurrenceString.java
@@ -0,0 +1,27 @@
+
+public class FindIndexFirstOccurrenceString {
+	
+	public int strStr(String haystack, String needle) {
+	    int n = haystack.length();
+	    int m = needle.length();
+
+	    if (m == 0) return 0;
+	    if (m > n) return -1;
+
+	    for (int i = 0; i <= n - m; i++) {
+	        int j = 0;
+	        while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+	            j++;
+	        }
+	        if (j == m) return i;
+	    }
+
+	    return -1;
+	}
+
+	public static void main(String[] args) {
+		var fifocs = new FindIndexFirstOccurrenceString();
+		System.out.println(fifocs.strStr("leetcode", "leeto"));
+	}
+
+}
