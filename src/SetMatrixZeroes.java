fb287da3e288d6075c63b19cad594d214dc365b1
João Martins <joaogasparlopesmartins@gmail.com>
2025-12-13 13:25:04 +0000

    solution O(n2) time O(1) space

--- src/SetMatrixZeroes.java
+++ src/SetMatrixZeroes.java
@@ -1,37 +1,65 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class SetMatrixZeroes {
 	
 	public void setZeroes(int[][] matrix) {
-		int rows = matrix.length;
-		int cols = matrix[0].length;
-		
-		List<Integer> rowsZero = new ArrayList<>();
-		List<Integer> colsZero = new ArrayList<>();
-		
-        for (int i = 0; i < rows; i++) {
-        	for (int j = 0; j < cols; j++)  {
-        		if (matrix[i][j] == 0) {
-        			rowsZero.add(i);
-        			colsZero.add(j);
-        		}
-        	}
-        }
-        
-        for (Integer row : rowsZero) {
-        	for (int k = 0; k < cols; k++) {
-        		matrix[row][k] = 0;
-        	}
-        }
-        
-        for (Integer col : colsZero) {
-        	for (int l = 0; l < rows; l++) {
-        		matrix[l][col] = 0;
-        	}
-        }
-        
-    }
+
+	    int rows = matrix.length;
+	    int cols = matrix[0].length;
+
+	    boolean firstRowZero = false;
+	    boolean firstColZero = false;
+
+	    for (int j = 0; j < cols; j++) {
+	        if (matrix[0][j] == 0) {
+	            firstRowZero = true;
+	            break;
+	        }
+	    }
+
+	    for (int i = 0; i < rows; i++) {
+	        if (matrix[i][0] == 0) {
+	            firstColZero = true;
+	            break;
+	        }
+	    }
+
+	    for (int i = 1; i < rows; i++) {
+	        for (int j = 1; j < cols; j++) {
+	            if (matrix[i][j] == 0) {
+	                matrix[i][0] = 0;
+	                matrix[0][j] = 0;
+	            }
+	        }
+	    }
+
+	    for (int i = 1; i < rows; i++) {
+	        if (matrix[i][0] == 0) {
+	            for (int j = 1; j < cols; j++) {
+	                matrix[i][j] = 0;
+	            }
+	        }
+	    }
+
+	    for (int j = 1; j < cols; j++) {
+	        if (matrix[0][j] == 0) {
+	            for (int i = 1; i < rows; i++) {
+	                matrix[i][j] = 0;
+	            }
+	        }
+	    }
+
+	    if (firstRowZero) {
+	        for (int j = 0; j < cols; j++) {
+	            matrix[0][j] = 0;
+	        }
+	    }
+
+	    if (firstColZero) {
+	        for (int i = 0; i < rows; i++) {
+	            matrix[i][0] = 0;
+	        }
+	    }
+	}
+
 
 	public static void main(String[] args) {
 		var smz = new SetMatrixZeroes();
