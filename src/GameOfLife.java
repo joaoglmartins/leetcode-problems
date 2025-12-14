3fa6e86611ce27bc0d31d085c13d42175b37cd24
João Martins <joaogasparlopesmartins@gmail.com>
2025-12-14 22:24:10 +0000

    solution O(n2) time O(1) space

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,69 +1,46 @@
-import java.util.Arrays;
-
 public class GameOfLife {
 	
 	public void gameOfLife(int[][] board) {
-		int rows = board.length;
-		int cols = board[0].length;
-        int[][] copy = Arrays.stream(board)
-                .map(int[]::clone)
-                .toArray(int[][]::new);
-        
-        for (int i = 0; i < rows; i++) {
-        	for (int j = 0; j < cols; j++) {
-        		int liveNbr = 0;
-        		boolean isAlive = copy[i][j] == 1; 
-        		
-        		if (i != 0 && j !=0) {
-        			liveNbr += copy[i-1][j-1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (i < rows-1 && j < cols-1) {
-        			liveNbr += copy[i+1][j+1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (i != 0 && j < cols-1) {
-        			liveNbr += copy[i-1][j+1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (i < rows-1 && j != 0) {
-        			liveNbr += copy[i+1][j-1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (i != 0) {
-        			liveNbr += copy[i-1][j] == 1 ? 1 : 0;
-        		}
-        		
-        		if (j != 0) {
-        			liveNbr += copy[i][j-1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (i < rows-1) {
-        			liveNbr += copy[i+1][j] == 1 ? 1 : 0;
-        		}
-        		
-        		if (j < cols-1) {
-        			liveNbr += copy[i][j+1] == 1 ? 1 : 0;
-        		}
-        		
-        		if (liveNbr < 2) {
-        			board[i][j] = 0;
-        			continue;
-        		}
-        		
-        		if (liveNbr < 4) {
-        			if (!isAlive) {
-        				board[i][j] = liveNbr == 3 ? 1 : 0;
-        			} else {
-        				board[i][j] = 1;
-        			}
-        			continue;
-        		}
-        		
-        		board[i][j] = 0;
-        	}
-        }
-    }
+	    int rows = board.length;
+	    int cols = board[0].length;
+
+	    for (int i = 0; i < rows; i++) {
+	        for (int j = 0; j < cols; j++) {
+
+	            int liveNbr = 0;
+
+	            for (int x = -1; x <= 1; x++) {
+	                for (int y = -1; y <= 1; y++) {
+	                    if (x == 0 && y == 0) continue;
+
+	                    int r = i + x;
+	                    int c = j + y;
+
+	                    if (r >= 0 && r < rows && c >= 0 && c < cols) {
+	                        liveNbr += board[r][c] & 1;
+	                    }
+	                }
+	            }
+
+	            boolean isAlive = (board[i][j] & 1) == 1;
+
+	            if (isAlive && (liveNbr < 2 || liveNbr > 3)) {
+	                board[i][j] = 2;
+	            }
+
+	            if (!isAlive && liveNbr == 3) {
+	                board[i][j] = 3;
+	            }
+	        }
+	    }
+
+	    for (int i = 0; i < rows; i++) {
+	        for (int j = 0; j < cols; j++) {
+	            board[i][j] >>= 1;
+	        }
+	    }
+	}
+
 
 	public static void main(String[] args) {
 		var gol = new GameOfLife();
