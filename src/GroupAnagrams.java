2baa5f348c907984a82a6ad54430fd8bde95cd15
João Martins <joaogasparlopesmartins@gmail.com>
2025-12-19 16:52:49 +0000

    solution O(n*k log k) time O(n*k) space

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -7,56 +7,18 @@ import java.util.Map;
 public class GroupAnagrams {
 
 	public List<List<String>> groupAnagrams(String[] strs) {
-		
-		if (strs.length == 0) return new ArrayList<>(Arrays.asList(Arrays.asList("")));
-		
-		Map<String, List<String>> strsMap = new HashMap<>();
-		
-		for (String str : strs) {
-			List<String> keys = new ArrayList<String>(strsMap.keySet());
-			
-			boolean added = false;
-			for (String key : keys) {
-				if (isAnagram(key, str)) {
-					List<String> value = strsMap.get(key);
-					value.add(str);
-					strsMap.put(key, value);
-					added = true;
-					break;
-				}
-			}
-			
-			if (added) continue;
-			
-			strsMap.put(str, Arrays.asList(str));
-		}
-		
-		List<List<String>> res = (List<List<String>>) strsMap.values();
-		
-		return res;
-		
+	    Map<String, List<String>> map = new HashMap<>();
+
+	    for (String s : strs) {
+	        char[] chars = s.toCharArray();
+	        Arrays.sort(chars);
+	        String key = new String(chars);
+
+	        map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+	    }
+
+	    return new ArrayList<>(map.values());
 	}
-	
-	private boolean isAnagram(String s, String t) {
-		
-		if (s.length() != t.length()) return false;
-		
-		int[] alpha = new int[26];
-		
-		for (int i = 0; i < s.length(); i++) {
-			char sChar = s.charAt(i);
-			char tChar = t.charAt(i);
-			
-			alpha[sChar - 'a'] += 1;
-			alpha[tChar - 'a'] -= 1;
-		}
-		
-		for (int n : alpha) {
-			if (n != 0) return false;
-		}
-		
-		return true;
-    }
 
 	public static void main(String[] args) {
 		var ga = new GroupAnagrams();
